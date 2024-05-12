7455aba3d4b508f8c030f0845ed4f4e598f4cdb9
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-12 22:18:24 +0900

    버그 패치
    발사체가 플레이어를 따라가는 버그
    플레이어가 간헐적으로 오브젝트를 파괴하지 못하는 버그

--- Assets/Scripts/Players/PlayerBeta.cs
+++ Assets/Scripts/Players/PlayerBeta.cs
@@ -11,14 +11,18 @@ public class PlayerBeta : Player {
     
     // 플레이어가 공격 키를 눌렀을 때를 감지합니다. 알파와 베타의 공격은 서로 다른 공격이기에 Player 클래스를 상속받는 자식 클래스에 작성하였습니다.
     void OnAttack() {
-        // 좌 또는 우방향을 보고 있음에 따라 발사체가 생성되는 위치를 지정합니다.
+        // 좌 또는 우방향을 보고 있음에 따라 발사체가 생성되는 위치와 방향을 지정합니다.
         pos = transform.position; // 현재 위치를 먼저 가져옵니다.
-        if (isLeft) pos.x = transform.position.x - 0.7f; // 좌측을 보고 있는 경우 현위치의 왼쪽으로 지정
-        else pos.x = transform.position.x + 0.7f; // 우측을 보고 있는 경우 현위치의 오른쪽으로 지정
+        if (isLeft) {
+            pos.x = transform.position.x - 0.7f; // 좌측을 보고 있는 경우 현위치의 왼쪽으로 지정
+        }
+        else {
+            pos.x = transform.position.x + 0.7f; // 우측을 보고 있는 경우 현위치의 오른쪽으로 지정
+        }
         spawnPoint.transform.position = pos; // 오브젝트의 위치를 위에서 지정된 값으로 지정합니다.
 
         // 오브젝트를 생성합니다.
-        GameObject obj = Instantiate(bulletPrefab, spawnPoint.transform);
+        GameObject obj = Instantiate(bulletPrefab, spawnPoint.transform.position, Quaternion.identity);
         obj.GetComponent<Bullet>().SetDirection(isLeft); // 오브젝트가 플레이어가 바라보는 방향으로 날아갑니다
     }
 }
