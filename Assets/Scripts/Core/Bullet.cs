b8e1b54aefaadc764b4d07cfc15ee0ee4fb57811
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-13 22:58:15 +0900

    장애물 코드 수정

--- Assets/Scripts/Core/Bullet.cs
+++ Assets/Scripts/Core/Bullet.cs
@@ -6,6 +6,10 @@ public class Bullet : MonoBehaviour {
     [Tooltip("발사체의 속도를 지정합니다.")]
     [SerializeField] float speed = 10;
     Vector3 moveDirection;
+    ElementType elementType = ElementType.None;
+
+    // 발사체의 속성을 지정합니다. 발사체가 속성을 가질 때에만 호출하여야 합니다.
+    public void SetElementType(ElementType elementType) { this.elementType = elementType; }
     
     // 발사체가 날아갈 방향을 지정하는 함수입니다. 외부에서 접근할 수 있게해 오브젝트가 생성될 때 정해질 수 있도록 합니다.
     public void SetDirection(bool isLeft) {
@@ -21,8 +25,13 @@ public class Bullet : MonoBehaviour {
 
     // 만약 어딘가에 닿았을 경우 오브젝트를 파괴합니다. 단, 오브젝트가 어딘가에 닿지 않았을 경우 파괴되지 않습니다.
     void OnTriggerEnter2D(Collider2D collision) { 
+        // 만약 스위치에 닿았을 경우 스위치를 작동시킵니다.
         if (collision.gameObject.TryGetComponent(out ISwitch component)) {
             component.Action();
+        } else if (collision.gameObject.TryGetComponent(out Block block) && 
+            elementType == ElementType.Fire) {
+            // 만약 발사체가 불속성일 때 잔디에 닿았을 경우 잔디를 파괴합니다.
+            if (block.IsBlockType(BlockType.Grass)) Destroy(collision.gameObject);
         }
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/Core/Enums.cs
@@ -0,0 +1,11 @@
+public enum BlockType {
+        Normal, // 기본값. 알파의 근거리 공격으로만 파괴 가능
+        Grass // 베타의 불 속성 원거리 공격으로만 파괴 가능
+};
+
+public enum ElementType {
+    None, // 기본값. 아무런 속성도 없습니다.
+    Fire, // 불 타입
+    Grass, // 풀 타입
+    Electrics // 전기 타입
+}
