aaf0fca7bc12011e85871dd1413e14db38408b24
leesy0828 <leesy0600@gmail.com>
2024-05-15 03:47:04 +0900

    Ball script

--- Assets/Scripts/Start/Ball.cs
+++ Assets/Scripts/Start/Ball.cs
@@ -1,13 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D Ball collision) {
-        // ball과 충돌했는지 확인합니다.
-        if (collision.gameObject.TryGetComponent(out  component)) {
-            component.Action(); // 스위치와 충돌한 경우 스위치 객체의 Action() 함수를 호출합니다.
+    bool playerA = false;
+    bool playerB = false;
+
+    public float maxSpeed = 1.0f;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "PlayerA")
+                playerA = true;
+
+            else if (collision.gameObject.name =="PlayerB")
+                playerB = true;
+
         }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "PlayerA")
+                playerA = false;
+
+            else if (collision.gameObject.name == "PlayerB")
+                playerB = false;
+        }
+
+        void MoveBall()
+    {
+        if (playerA == true && playerB == true)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+            
+        else
+            rb.velocity = new Vector2(0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveBall();
     }
 }
