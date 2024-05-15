eade66eb19949a131b2b3620fb039c6487680fc6
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-15 13:21:17 +0900

    플레이어 버그 수정

--- Assets/Scripts/Core/GroundCheck.cs
+++ Assets/Scripts/Core/GroundCheck.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ public class GroundCheck : MonoBehaviour {
     [SerializeField] Player player;
     [Tooltip("점프 가능 여부를 확인하기 위한 바닥 오브젝트의 태그 이름입니다.")]
     [SerializeField] LayerMask groundLayer;
+    [Tooltip("점프 가능 여부를 확인하기 위한 플레이어 오브젝트의 태그 이름입니다.")]
+    [SerializeField] LayerMask playerLayer;
     Vector3 pos; // 플레이어의 위치를 저장합니다.
     
     void Update() {
@@ -19,6 +22,13 @@ public class GroundCheck : MonoBehaviour {
         pos.x -= 0.5f; // Ray의 시작좌표를 설정합니다.
         pos.y -= 0.5f;
         for (int i = 0; i < 3; i++) { // 플레이어의 맨 앞, 중앙, 맨 뒤로 x좌표를 지정합니다.
+            // 현재 플레이어를 밟은 상태인지 확인합니다.
+            RaycastHit2D playerCheck = Physics2D.Raycast(new Vector2(pos.x + (0.5f * i), pos.y), Vector2.down, 0.2f, playerLayer);
+            if (playerCheck.collider != null && playerCheck.collider.gameObject != player.gameObject) {
+                player.SetGrounded(true);
+                break;
+            }
+
             RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(pos.x + (0.5f * i), pos.y), Vector2.down, 0.2f, groundLayer); // 플레이어의 아래쪽으로 가상의 Ray를 발사해 바닥이 존재하는지 확인합니다.
             if (hits.Length != 0) { // 조건에 만족하는 오브젝트 수가 0 이상인 경우 바닥에 닿은 것으로 판정합니다.
                 player.SetGrounded(true);
