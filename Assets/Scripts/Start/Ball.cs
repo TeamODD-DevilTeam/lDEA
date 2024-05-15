2327ebe5c7a4b1becd388c001ec1abd838cd6a80
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-15 12:43:15 +0900

    Ball 스크립트 개선

--- Assets/Scripts/Start/Ball.cs
+++ Assets/Scripts/Start/Ball.cs
@@ -3,50 +3,34 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
-    bool playerA = false;
-    bool playerB = false;
-
+public class Ball : MonoBehaviour {
+    [Tooltip("충돌을 감지할 오브젝트입니다.")]
+    [SerializeField] GameObject playerA, playerB;
+    [Tooltip("이 오브젝트의 Rigidbody2D입니다.")]
+    [SerializeField] Rigidbody2D rb;
+    bool isA = false;
+    bool isB = false;
     public float maxSpeed = 1.0f;
-    private Rigidbody2D rb;
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject == playerA) isA = true;
+        else if (collision.gameObject == playerB) isB = true;
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.name == "PlayerA")
-                playerA = true;
-
-            else if (collision.gameObject.name =="PlayerB")
-                playerB = true;
-
-        }
-
-        void OnCollisionExit2D(Collision2D collision)
-        {
-            if (collision.gameObject.name == "PlayerA")
-                playerA = false;
-
-            else if (collision.gameObject.name == "PlayerB")
-                playerB = false;
-        }
-
-        void MoveBall()
-    {
-        if (playerA == true && playerB == true)
-        {
-            rb.bodyType = RigidbodyType2D.Dynamic;
-        }
-            
-        else
-            rb.velocity = new Vector2(0, 0);
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject == playerA) isA = false;
+        else if (collision.gameObject == playerB) isB = false;
     }
 
-    private void FixedUpdate()
-    {
+    void MoveBall() {
+        if (isA && isB) rb.bodyType = RigidbodyType2D.Dynamic;
+        else {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void FixedUpdate() {
         MoveBall();
     }
 }
