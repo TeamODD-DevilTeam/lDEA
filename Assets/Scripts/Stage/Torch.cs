5828a80213d1f4b4780942c6e0b0d148c8dafb93
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-23 23:35:00 +0900

    5스테이지 수정

--- Assets/Scripts/Stage/Torch.cs
+++ Assets/Scripts/Stage/Torch.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Torch : Block {
-    [Tooltip("이 오브젝트의 스프라이트 렌더러입니다.")]
-    [SerializeField] SpriteRenderer sprite;
-    [Tooltip("기본 횃불 스프라이트입니다.")]
-    [SerializeField] Sprite normalTorch;
-    [Tooltip("활성화된 횃불 스프라이트입니다.")]
-    [SerializeField] Sprite activedTorch;
+    [Tooltip("이 오브젝트의 애니메이터입니다.")]
+    [SerializeField] Animator animator;
     
     bool isActive = false; // 횃불의 상태를 저장하는 변수입니다.
 
     // 횃불이 비활성화된 상태라면 활성화합니다.
     public void Active() {
         if (!isActive) {
-            sprite.sprite = activedTorch;
+            animator.SetBool("isFire", true);
             isActive = true;
         }
     }
