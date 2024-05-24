8b451fd02b24f550d38441ad611b114cc887e718
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-05-24 15:54:30 +0900

    스테이지 그만 수정하고 싶다

--- Assets/Scripts/Loading/LoadingManager.cs
+++ Assets/Scripts/Loading/LoadingManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LoadingManager : MonoBehaviour {
     [SerializeField] Animator animator;
     [SerializeField] List<string> guides = new List<string>();
+    [SerializeField] TMP_Text guide_text;
     public static LoadingManager instance;
 
     void Start() {
         instance = this;
+        guide_text.text = guides[Random.Range(0, guides.Count - 1)];
     }
 }
